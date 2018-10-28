5f343cd59687e328ea086933190d43b35c9c501b
Jeferson Urbieta <jeferson.urbieta@gmail.com>
2018-10-28 13:03:25 -0400

    Alterações para teste de cross

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 var createError = require('http-errors');
 var express = require('express');
+var bodyParser = require('body-parser')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require('dotenv');
-
-
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.load();
-}
 
 var indexRouter = require('./routes/index');
 var contactRouter = require('./routes/contact');
 
 var app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.load();
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb', type: 'application/json'}));
+app.use(bodyParser.urlencoded({
+  parameterLimit: 100000,
+  limit: '50mb',
+  extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.bodyParser({limit: '50mb'}));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
