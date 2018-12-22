dd933151cf928a198bf8289791277fda96e51c97
Jeferson Urbieta <jeferson.urbieta@gmail.com>
2018-12-22 19:25:43 -0300

    Alterações no email para apresentar latitude e logitude

--- emails/sendEmail.js
+++ emails/sendEmail.js
@@ -45,7 +45,7 @@ exports.sendEmail = function (subject, dados) {
   var transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASSWORD
