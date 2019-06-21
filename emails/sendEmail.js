93238ff7460f8cad7e34305ea081e8b3f20a5146
Jeferson Urbieta <jeferson.urbieta@gmail.com>
2019-06-21 12:08:00 -0400

    teste

--- emails/sendEmail.js
+++ emails/sendEmail.js
@@ -45,10 +45,13 @@ exports.sendEmail = function (subject, dados) {
   var transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASSWORD
+    },
+    tls: {
+      ciphers: 'SSLv3'
     }
   });
 
